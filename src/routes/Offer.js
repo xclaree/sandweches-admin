2801256de6116a0401f6907139bbb8f224ff55d1
Marcelloallegrezza <90706479+Marcelloallegrezza@users.noreply.github.com>
2023-02-15 12:25:53 +0100

    Update Offer.js

--- src/routes/Offer.js
+++ src/routes/Offer.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import OfferTable from "../components/OfferTable";
 
 function Offer(){
-    return(
-        <div className="home">
-            <h1>Offer</h1>
-        </div>
+    return (
+        <>
+            <div className="home" >
+                <div>
+                    <h1>Offer</h1>
+                </div>
+                <span></span>
+                <div className="offer-list">
+                    <div>
+                        <OfferTable /> 
+                    </div>
+
+                </div>
+            </div>
+
+        </>
     )
 }
 
