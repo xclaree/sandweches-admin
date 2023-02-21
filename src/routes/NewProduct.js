d349f305c24101245c3929b0a8f65c122b185636
val <valeriabaleanu@gmail.com>
2023-02-21 17:06:02 +0100

    miglioramento newproduct (add product)

--- src/routes/NewProduct.js
+++ src/routes/NewProduct.js
@@ -1,16 +1,28 @@
 import { margin } from "@mui/system";
 import React, { useState } from "react";
 import "../App.css";
+import { ScrollView, View, Text } from "react-native";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { getIngredient } from "../api/prova";
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
-const categories = ["Category 1", "Category 2", "Category 3"];
+const categories = ["Panini", "Piadine", "Bevande"]; //API QUI
+
+const ingredients = [ //API QUI
+  "Pane",
+  "Prosciutto",
+  "Salame",
+  "Mortadella",
+  "Formaggio",
+  "Bresaola",
+];
 
 function NewProduct() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState(categories[0]);
+  const [ingredient, setIngredient] = useState(ingredients[0]);
   const [quantity, setQuantity] = useState(0);
   const [kcal, setKcal] = useState(0);
   const [fat, setFat] = useState(0);
@@ -21,6 +33,7 @@ function NewProduct() {
   const [fiber, setFiber] = useState(0);
   const [salt, setSalt] = useState(0);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -42,6 +55,19 @@ function NewProduct() {
     setCategory(event.target.value);
   };
 
+  const handleIngredientClick = (index) => {
+    const selectedIndex = selectedOptions.indexOf(index);
+    let newSelectedOptions = [...selectedOptions];
+
+    if (selectedIndex === -1) {
+      newSelectedOptions.push(index);
+    } else {
+      newSelectedOptions.splice(selectedIndex, 1);
+    }
+
+    setSelectedOptions(newSelectedOptions);
+  };
+
   const handleQuantityChange = (event) => {
     setQuantity(parseInt(event.target.value));
   };
@@ -51,32 +77,36 @@ function NewProduct() {
   };
 
   const handleQuantityDecrement = () => {
-    setQuantity(quantity - 1);
+    if (quantity == 0) {
+      setQuantity(0);
+    } else {
+      setQuantity(quantity - 1);
+    }
   };
 
   const handleKclaChange = (event) => {
     setKcal(parseInt(event.target.value));
   };
   const handleFatChange = (event) => {
-    setFat(parseInt(event.target.value));
+    setFat(parseFloat(event.target.value));
   };
   const handleSaturedFatChange = (event) => {
-    setSaturedFat(parseInt(event.target.value));
+    setSaturedFat(parseFloat(event.target.value));
   };
   const handleCarbohydrateChange = (event) => {
-    setCarbohydrate(parseInt(event.target.value));
+    setCarbohydrate(parseFloat(event.target.value));
   };
   const handleSugarChange = (event) => {
-    setSugar(parseInt(event.target.value));
+    setSugar(parseFloat(event.target.value));
   };
   const handleProteinChange = (event) => {
-    setProtein(parseInt(event.target.value));
+    setProtein(parseFloat(event.target.value));
   };
   const handleFiberChange = (event) => {
-    setFiber(parseInt(event.target.value));
+    setFiber(parseFloat(event.target.value));
   };
   const handleSaltChange = (event) => {
-    setSalt(parseInt(event.target.value));
+    setSalt(parseFloat(event.target.value));
   };
   const handleItemPress = (item) => {
     setSelectedItem(item);
@@ -90,183 +120,205 @@ function NewProduct() {
     },
   });
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1 className="home">
-        Aggiungi prodotto
-        </h1>
-        <div>
-          <label style={{ position: "fixed", top: "7%", right: "40%" }}>
-            Name:
-            <input type="text" value={name} onChange={handleNameChange} />
-          </label>
-        </div>
-        <br />
-        <label style={{ position: "fixed", top: "18%", left: "35%" }}>
-          Description:
-          <textarea
-            value={description}
-            onChange={handleDescriptionChange}
-            style={{ resize: "none" }}
-          />
-        </label>
-        <br />
-        <label style={{ position: "fixed", top: "21%", right: "33.7%" }}>
-          Price:
-          <input
-            type="number"
-            step="0.01"
-            value={price}
-            onChange={handlePriceChange}
-          />
-        </label>
-        <br />
-        <label style={{ position: "fixed", top: "35%", left: "35%" }}>
-          Category:
-          <select value={category} onChange={handleCategoryChange}>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label style={{ position: "fixed", top: "35%", right: "30%" }}>
-          Quantity:
-          <div>
-            <input
-              type="number"
-              min="0"
-              max="99"
-              value={quantity}
-              onChange={handleQuantityChange}
-            />
-            <button type="button" onClick={handleQuantityIncrement}>
-              +
-            </button>
-            <button type="button" onClick={handleQuantityDecrement}>
-              -
-            </button>
-          </div>
-        </label>
-        <br />
-        <div className="addIngredient">
-          <h3>INGREDIENTI</h3>
-        </div>
+    <>
+      <div className="home">
+        <h1>Aggiungi prodotto</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <div>
+              <label>
+                Nome prodotto:
+                <input
+                  style={{ width: "300px" }}
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                  placeholder="Inserire il nome del prodotto"
+                />
+              </label>
+              <span></span>
+              <label>
+                Prezzo:
+                <input
+                  type="number"
+                  step="0.01"
+                  value={price}
+                  onChange={handlePriceChange}
+                  placeholder="Inserire il prezzo"
+                  style={{ width: "150px" }}
+                />
+              </label>
 
-        <div className="infoValoriNutrizionali">
-          <label className="lbNutritionalValues">NUTRITIONAL VALUES</label>
-          <div className="infoKcal">
-            <label>
-              KCAL
-              <input
-                type="number"
-                min="0"
-                value={kcal}
-                onChange={handleKclaChange}
-              />
-            </label>
-          </div>
+              <span></span>
+              <label>
+                Categoria:
+                <select
+                  value={category}
+                  onChange={handleCategoryChange}
+                  style={{ width: "auto" }}
+                >
+                  {categories.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <span></span>
+              <label>
+                Quantità:
+                <div>
+                  <button
+                    className="btn-form"
+                    onClick={handleQuantityIncrement}
+                  >
+                    +
+                  </button>
+                  <input
+                    type="number"
+                    min="0"
+                    max="99"
+                    value={quantity}
+                    onChange={handleQuantityChange}
+                    style={{ textAlign: "center" }}
+                  />
 
-          <div className="infoFat">
-            <label>
-              FAT
-              <input
-                type="number"
-                min="0"
-                value={fat}
-                onChange={handleFatChange}
-              />
-            </label>
-          </div>
+                  <button
+                    className="btn-form"
+                    onClick={handleQuantityDecrement}
+                  >
+                    -
+                  </button>
+                </div>
+              </label>
+            </div>
+            <span style={{ width: "90px" }}></span>
+            <div>
+              <div style={{ display: "flex" }}>
+                <label>
+                  Descrizione:
+                  <textarea
+                    style={{ width: "300px", resize: "none" }}
+                    value={description}
+                    onChange={handleDescriptionChange}
+                    placeholder="Inserire una breve descrizione del prodotto"
+                  />
+                </label>
+                <span style={{ width: "100px"}}></span>
+                <div>
+                <a>Scegli ingredienti</a>
+                <div style={{ height: "130px", width: "200px", overflowY: "scroll" }}>
+                  {ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                      <input
+                        type="checkbox"
+                        checked={selectedOptions.includes(index)}
+                        onChange={() => handleIngredientClick(index)}
+                      />
+                      {ingredient}
+                    </div>
+                  ))}
+                  {/* <p>Selected options indexes: {selectedOptions.join(", ")}</p> */}
+                </div>
+                </div>
+              </div>
+              <span></span>
 
-          <div className="infoSaturedFat">
-            <label>
-              SATURED FAT
-              <input
-                type="number"
-                min="0"
-                value={saturedFat}
-                onChange={handleSaturedFatChange}
-              />
-            </label>
-          </div>
+              <div>Valori nutrizionali: </div>
 
-          <div className="infoCarbohydrate">
-            <label>
-              CARBOHYDRATE
-              <input
-                type="number"
-                min="0"
-                value={carbohydrate}
-                onChange={handleCarbohydrateChange}
-              />
-            </label>
-          </div>
+              <div className="nutritional-values">
+                <label>
+                  kcal
+                  <input
+                    type="number"
+                    min="0"
+                    value={kcal}
+                    onChange={handleKclaChange}
+                  />
+                </label>
 
-          <div className="infoSugar">
-            <label>
-              SUGAR
-              <input
-                type="number"
-                min="0"
-                value={sugar}
-                onChange={handleSugarChange}
-              />
-            </label>
-          </div>
+                <label>
+                  Grassi
+                  <input
+                    type="number"
+                    min="0"
+                    pattern="^\d*(\.\d{0,2})?$"
+                    value={fat}
+                    onChange={handleFatChange}
+                  />
+                </label>
 
-          <div className="infoProtein">
-            <label>
-              PROTEIN
-              <input
-                type="number"
-                min="0"
-                value={protein}
-                onChange={handleProteinChange}
-              />
-            </label>
-          </div>
+                <label>
+                  Grassi saturi
+                  <input
+                    type="number"
+                    min="0"
+                    value={saturedFat}
+                    onChange={handleSaturedFatChange}
+                  />
+                </label>
 
-          <div className="infoFiber">
-            <label>
-              FIBER
-              <input
-                type="number"
-                min="0"
-                value={fiber}
-                onChange={handleFiberChange}
-              />
-            </label>
-          </div>
+                <label>
+                  Carboidrati
+                  <input
+                    type="number"
+                    min="0"
+                    value={carbohydrate}
+                    onChange={handleCarbohydrateChange}
+                  />
+                </label>
+              </div>
+              <div className="nutritional-values">
+                <label>
+                  Zuccheri
+                  <input
+                    type="number"
+                    min="0"
+                    value={sugar}
+                    onChange={handleSugarChange}
+                  />
+                </label>
 
-          <div className="infoSalt">
-            <label>
-              SALT
-              <input
-                type="number"
-                min="0"
-                value={salt}
-                onChange={handleSaltChange}
-              />
-            </label>
+                <label>
+                  Proteine
+                  <input
+                    type="number"
+                    min="0"
+                    value={protein}
+                    onChange={handleProteinChange}
+                  />
+                </label>
+
+                <label>
+                  Fibre
+                  <input
+                    type="number"
+                    min="0"
+                    value={fiber}
+                    onChange={handleFiberChange}
+                  />
+                </label>
+
+                <label>
+                  Sale
+                  <input
+                    type="number"
+                    min="0"
+                    value={salt}
+                    onChange={handleSaltChange}
+                  />
+                </label>
+              </div>
+            </div>
           </div>
-        </div>
-        <button
-          type="submit"
-          style={{ position: "fixed", bottom: "5%", right: "41%" }}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+          <button className="submit">Aggiungi</button>
+        </form>
+      </div>
+    </>
   );
 }
 
 export default NewProduct;
 /*
 manca mettere API crea prodotto
-manca la selkezione e deselezione del prodotto
-manca centrare label ingredienti, satured fat e Carbohydrate
 */
