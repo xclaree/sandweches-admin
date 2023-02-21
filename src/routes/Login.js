82920f44a89256cc6d8d43b2a56f62e9a916f310
val <valeriabaleanu@gmail.com>
2023-02-21 10:33:04 +0100

    Revert "modifica login"
    This reverts commit 48ad9203e237a1d2cdc0fa95c10ab079bf60ce01.

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,116 +1,26 @@
-import { useRef, useEffect, useContext } from 'react';
-import { createContext, useState } from "react";
-
-const AuthContext = createContext({});
-
-export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
-
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-const Login = () => {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
-
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
-
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        // try {
-        //     const response = await axios.post(
-        //         JSON.stringify({ user, pwd }),
-        //         {
-        //             headers: { 'Content-Type': 'application/json' },
-        //             withCredentials: true
-        //         }
-        //     );
-        //     console.log(JSON.stringify(response?.data));
-        //     //console.log(JSON.stringify(response));
-        //     const accessToken = response?.data?.accessToken;
-        //     const roles = response?.data?.roles;
-        //     setAuth({ user, pwd, roles, accessToken });
-        //     setUser('');
-        //     setPwd('');
-        //     setSuccess(true);
-        // } catch (err) {
-        //     if (!err?.response) {
-        //         setErrMsg('No Server Response');
-        //     } else if (err.response?.status === 400) {
-        //         setErrMsg('Missing Username or Password');
-        //     } else if (err.response?.status === 401) {
-        //         setErrMsg('Unauthorized');
-        //     } else {
-        //         setErrMsg('Login Failed');
-        //     }
-        //     errRef.current.focus();
-        // }
+import { display } from "@mui/system";
+import React from "react";
+import { Link } from 'react-router-dom';
+import App from'./NewProduct';
+    const nextPage = () => {
+      window.location.href = "./NewProduct.js";
     }
-
-    return (
-        <>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
-                    <p>
-                        <a href="#">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
-
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            {/*put router link here*/}
-                            <a href="#">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
-        </>
+function Product(){
+    return(
+<div className="home" onClick={nextPage}>
+            <h1>LOGIN</h1>                    
+            <label className="label_login_in_your_account">Log in your account</label>
+            <div>
+            <input name="user_email" type="text" value="Email" size="40" maxlength="200" className="input_user_email" />
+            </div>
+            <div>
+            <input name="user_password" type="text" value="Password" size="40" maxlength="200" className="input_user_password" />
+            </div>
+            <div className="button_login">
+                Log in
+            </div>
+            <label className="label_forget_password">Forget your password</label>
+        </div> 
     )
 }
-
-export default Login
+export default Product;
