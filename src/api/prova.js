47f59eaf2f752d722c9d751288e5fe496d159f98
Formenton Matteo <90769483+Formenton04@users.noreply.github.com>
2023-02-22 18:20:28 +0100

    Update prova.js

--- src/api/prova.js
+++ src/api/prova.js
@@ -20,3 +20,15 @@ export function getProduct(id) {
         .get("https://paninara.claudiodressadore.net/evomatic/API/order/getArchiveOrder.php")
         .then(res=> res.data)
     }
+
+    export function getArchiveProduct(){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/product/getArchiveProductUnfiltered.php")
+        .then(res=>res.data)
+    }
+
+    export function getArchiveOffer(){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/offer/getArchiveOffer.php")
+        .then(res=>res.data)
+    }
