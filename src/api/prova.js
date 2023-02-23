8e64a477766e6e4e36db59f69687aea9ce500fb9
Formenton Matteo <90769483+Formenton04@users.noreply.github.com>
2023-02-23 21:54:27 +0100

    Update prova.js

--- src/api/prova.js
+++ src/api/prova.js
@@ -1,52 +1,33 @@
 import axios from "axios";
 
-export function getIngredient() {
-  return axios
-    .get(
-      "https://paninara.claudiodressadore.net/evomatic/API/product/ingredient/getArchiveIngredient.php"
-    )
-    .then((res) => res.data);
-}
-export function getProduct(id) {
-  return axios
-    .get(
-      "https://paninara.claudiodressadore.net/evomatic/API/product/getProductUnfiltered.php",
-      {
-        params: {
-          id: id,
-        },
-      }
-    )
-    .then((res) => res.data);
+
+export function getIngredient(){
+    return axios
+    .get("https://paninara.claudiodressadore.net/evomatic/API/product/ingredient/getArchiveIngredient.php")
+    .then(res=> res.data)
 }
 
-export function getArchiveOrder() {
-  return axios
-    .get(
-      "https://paninara.claudiodressadore.net/evomatic/API/order/getArchiveOrder.php"
-    )
-    .then((res) => res.data);
-}
 
-export function setLogin(email, password) {
-  return axios
-    .get("https://paninara.claudiodressadore.net/evomatic/API/user/login.php", {
-      params: {
-        email: email,
-        password: password,
-      },
-    })
-    .then((res) => res.data); //ritorna l'id della persona
-}
+    export function getArchiveOrder(){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/order/getArchiveOrder.php")
+        .then(res=> res.data)
+    }
 
-export function getArchiveProduct(){
-  return axios
-  .get("https://paninara.claudiodressadore.net/evomatic/API/product/getArchiveProductUnfiltered.php")
-  .then(res=>res.data)
-}
+    export function getArchiveProduct(){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/product/getArchiveProductUnfiltered.php")
+        .then(res=>res.data)
+    }
 
-export function getArchiveOffer(){
-  return axios
-  .get("https://paninara.claudiodressadore.net/evomatic/API/offer/getArchiveOffer.php")
-  .then(res=>res.data)
-}
+    export function getArchiveOffer(){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/offer/getArchiveOffer.php")
+        .then(res=>res.data)
+    }
+
+    export function getProductAllergens(id){
+        return axios
+        .get("https://paninara.claudiodressadore.net/evomatic/API/product/getProductAllergens.php"+id)
+        .then(res=> res.data)
+    }
