096dabf031364062d8a453d920e485306ff66418
val <90759719+xclaree@users.noreply.github.com>
2023-02-25 14:42:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./routes/Home";
 import Ingredient from "./routes/Ingredient";
 import Product from "./routes/Product";
@@ -11,67 +10,114 @@ import NewProduct from "./routes/NewProduct";
 import TermsConditions from "./routes/TermsConditions";
 import ResetPassword from "./routes/ResetPassword";
 import ProductDetails from"./routes/ProductDetails";
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import "./App.css";
+import Login from "./routes/Login";
 import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { useState } from "react";
 
-const AppLayout = () => (
-  <>
-    <Outlet />
-    <NavBar />
-  </>
-);
+function App() {
+  const [token, setToken] = useState(0);
 
-const queryClient = new QueryClient();
+  if (token == 0) {
+    return <Login setToken={token} />;
+  }
 
-const router = createBrowserRouter([ 
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "product",
-        element: <Product />,
-      },
-      {
-        path: "ingredient",
-        element: <Ingredient />,
-      },
-      {
-        path: "offer",
-        element: <Offer />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-      },
-      {
-        path: "newproduct",
-        element: <NewProduct />
-      },
-      {
-        path: "termsconditions",
-        element: <TermsConditions />
-      },
-      {
-        path: "resetpassword",
-        element: <ResetPassword />
-      },
-      {
-        path: "Dettagli Prodotto",
-        element: <ProductDetails />
-      }
-    ],
-  },
-]);
+  return (
+    <>
+      <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/offer" element={<Offer />} />
+              <Route path="/ingredient" element={<Ingredient />} />
+              <Route path="/profile" element={<Profile />}/>
+              <Route path="/newproduct" element={<NewProduct />} />
+            </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+
+
+const clientQuery = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={clientQuery}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
+
+
+
+
+
+// import "./App.css";
+
+
+// const AppLayout = () => (
+//   <>
+//     <Outlet />
+//     <NavBar />
+//   </>
+// );
+
+// const queryClient = new QueryClient();
+
+// const router = createBrowserRouter([ 
+//   {
+//     element: <AppLayout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "product",
+//         element: <Product />,
+//       },
+//       {
+//         path: "ingredient",
+//         element: <Ingredient />,
+//       },
+//       {
+//         path: "offer",
+//         element: <Offer />,
+//       },
+//       {
+//         path: "profile",
+//         element: <Profile />,
+//       },
+//       {
+//         path: "newproduct",
+//         element: <NewProduct />
+//       },
+//       {
+//         path: "termsconditions",
+//         element: <TermsConditions />
+//       },
+//       {
+//         path: "resetpassword",
+//         element: <ResetPassword />
+//       },
+//       {
+//         path: "Dettagli Prodotto",
+//         element: <ProductDetails />
+//       }
+//     ],
+//   },
+// ]);
+
+// createRoot(document.getElementById("root")).render(
+//   <QueryClientProvider client={queryClient}>
+//     <RouterProvider router={router} />
+//   </QueryClientProvider>
+// );
